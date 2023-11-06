b376350aafa4b90bf12800a56e7868892d981975
JAngelGL <75130694+JAngelGL@users.noreply.github.com>
2023-11-05 22:31:01 -0600

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -18,8 +18,9 @@ model = tf.keras.models.load_model('lol_champion_classifier3.h5')
 champion_names = ['atroxx', 'ahir', 'akali', 'akshan','alistar' ]  # Agrega el nombre de tus campeones aquí en el mismo orden que se usó para el entrenamiento
 
 # Cargar una imagen para probar (reemplaza 'imagen_a_probar.jpg' con la ruta de tu imagen)
-img_path = '/content/aatroxturn.jpg'
-#img_path = '/content/ability_0103_W1.jpg'
+img_path = '/content/aatroxturn.jpg' #Cambiar a la ruta del archivo
+
+#img_path = '/test_images'
 img = image.load_img(img_path, target_size=(150, 150))
 img_array = image.img_to_array(img)
 img_array = np.expand_dims(img_array, axis=0)
